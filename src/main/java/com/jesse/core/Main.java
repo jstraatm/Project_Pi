e1f9e56ff1ff31157e39e153b08834c89bbb4149
Jesse <jesse.straatman@capgemini.com>
2015-11-18 21:54:52 +0100

    Updated pom Added main.java and maintest.java

---
+++ src/main/java/com/jesse/core/Main.java
@@ -0,0 +1,8 @@
+package com.jesse.core;
+
+public class Main{
+
+    public static void main(String[] args ) {
+        System.out.println("Ik ben gestart ggg");
+    }
+}
---
+++ src/test/java/com/jesse/core/MainTest.java
@@ -0,0 +1,17 @@
+package com.jesse.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest{
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testDit() {
+
+    }
+}
